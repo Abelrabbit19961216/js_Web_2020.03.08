26de6f9b86045ebd9f772769d4a938a96431ed41
Abelrabbit19961216 <>
2021-03-08 21:36:38 +0800

    更新

---
+++ main.js
@@ -0,0 +1,14 @@
+var index = 0;
+
+var nexBth = document .querySelector("#slider-next");
+
+var items =document.grtElenentaBtClassName("slider-item");
+
+function next () {
+    index++;
+    if (index == items.length) {
+        index -0;
+    }
+
+
+nextBth.onclick = next;
